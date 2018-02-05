7c527532a4f465c91177cd7fa1200d75801c4639
JoostSijm <joostsijm@gmail.com>
2018-02-05 20:25:20 +0100

    Update init file with comments and whitespace

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 
-from flask import Flask, render_template, jsonify
+"""
+Website for Supremacy-stats
+"""
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object(__name__)
 
+app = Flask(__name__)
+
+app.config.from_object(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgresql://supindex@localhost/supindex'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
