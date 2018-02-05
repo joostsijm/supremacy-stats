f9b22b1d1e4d6914b7de8f8348f5ae45915291d8
JoostSijm <joostsijm@gmail.com>
2018-02-05 20:23:04 +0100

    Update routes with homepage

--- app/flaskr.py
+++ app/flaskr.py
@@ -8,9 +8,14 @@ from app import app
 from app.models.game import Game
 
 
-@app.route('//')
-@app.route('/<game_id>')
-def index(game_id=None):
+@app.route('/')
+def index():
+    """Show homepage"""
+    return render_template('index.html', Game=Game)
+
+
+@app.route('/game/<game_id>')
+def game(game_id=None):
     """Return index"""
 
     if game_id is not None:
@@ -20,13 +25,12 @@ def index(game_id=None):
             game_id = None
 
     game = Game.query.filter(Game.game_id == game_id).first()
-
     return render_template('game.html', game=game)
 
 
 @app.route('/api/game/<game_id>')
-def game(game_id):
-    """Return api results for game statics"""
+def api_game(game_id):
+    """Return results for game statics"""
 
     try:
         game_id = int(game_id)
