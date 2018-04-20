710dfe8bb67ecff7997c66e84595923094ddbae9
JoostSijm <joostsijm@gmail.com>
2018-04-20 14:12:27 +0200

    Fix issue with changing force_relation name

--- app/flaskr.py
+++ app/flaskr.py
@@ -67,15 +67,15 @@ def game_relations(game_id):
     return render_template('game/relations.html', game=game)
 
 
-@app.route('/game/<int:game_id>/force_relations')
+@app.route('/game/<int:game_id>/relations_2')
 @register_breadcrumb(app, '.games.game_id', '',
                      dynamic_list_constructor=game_overview_dlc)
-def game_force_relations(game_id):
+def game_relations_2(game_id):
     """Show game relations"""
 
     game_id = int(game_id)
     game = Game.query.filter(Game.game_id == game_id).first()
-    return render_template('game/force_relations.html', game=game)
+    return render_template('game/relations_2.html', game=game)
 
 @app.route('/game/<int:game_id>/edge_relations')
 @register_breadcrumb(app, '.games.game_id', '',
