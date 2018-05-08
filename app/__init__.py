d36e6a10737da163d2c7df83507542eea2b0b2c5
JoostSijm <joostsijm@gmail.com>
2018-05-08 16:01:26 +0200

    Add compress extention

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from github_webhook import Webhook
 from flask_migrate import Migrate
+from flask_compress import Compress
 
 
 app = Flask(__name__)
@@ -16,6 +17,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgresql://supindex@localhost/supindex'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+COMPRESS_MIMETYPES = ['text/html', 'text/css', 'text/xml', 'application/json', 'application/javascript']
+COMPRESS_LEVEL = 6
+COMPRESS_MIN_SIZE = 500
+Compress(app)
 
 db = SQLAlchemy(app)
 webhook = Webhook(app)
