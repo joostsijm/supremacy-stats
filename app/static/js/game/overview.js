24bf206dbe73365ce403481a1c8b71053f5ff4c7
JoostSijm <joostsijm@gmail.com>
2018-11-30 18:25:23 +0100

    Working on improving the game overview page

--- app/static/js/game/overview.js
+++ app/static/js/game/overview.js
@@ -87,3 +87,46 @@ function labelFunction(item, label)
 		return ""
 }
 
+$(document).ready(function() {
+	$('.dataTable.countrys').DataTable({
+		order: [4, 'desc'],
+		responsive: {
+			details: {
+				type: 'column',
+				target: 'tr td:not(:first-child)'
+			}
+		},
+		paging: false,
+		columnDefs: [
+			{ 
+				targets: [5],
+				orderData:[6],
+			},
+			{
+				targets: [6],
+				visible: false,
+				searchable: false,
+				type: 'non-empty-string',
+			},
+		],
+	});
+});
+
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    'non-empty-string-asc': function (str1, str2) {
+        if(str1 == '')
+            return 1;
+        if(str2 == '')
+            return -1;
+        return ((str1 < str2) ? -1 : ((str1 > str2) ? 1 : 0));
+    },
+ 
+    'non-empty-string-desc': function (str1, str2) {
+        if(str1 == '')
+            return 1;
+        if(str2 == '')
+            return -1;
+        return ((str1 < str2) ? 1 : ((str1 > str2) ? -1 : 0));
+    }
+});
+
