5fc71232f623df695efce6bb2bcff1952e03dde0
JoostSijm <joostsijm@gmail.com>
2018-12-07 17:06:51 +0100

    Split market job and job to base class

--- app/util/job.py
+++ app/util/job.py
@@ -5,51 +5,27 @@ from app import scheduler
 from app.models import Game
 
 
-class Job():
-    """Class for handling scheduler"""
+class BaseJob():
+    """Base class for jobs"""
 
     game = None
-    job = None
-    market_job = None
     game_id = None
+    job = None
 
     def __init__(self, game):
-        """Initialize api"""
+        """Standard constructor for jobs"""
         self.game = game
         self.game_id = str(game.game_id)
-        self.job = scheduler.get_job(self.game_id)
-        self.market_job = scheduler.get_job(self.game_id)
 
     def start(self):
-        """Start job for game"""
-        scheduler.add_job(
-            id=self.game_id,
-            func=run,
-            args=[self.game.game_id],
-            trigger="interval",
-            days=1,
-            start_date=self.game.next_day_time + timedelta(minutes=5)
-        )
-
-
-    def start_market(self):
-        """Start job for market"""
-        scheduler.add_job(
-            id="market_%s" % self.game_id,
-            func=run_market,
-            args=[self.game.game_id],
-            trigger="interval",
-            hours=1,
-        )
-
+        """Start the scheduler"""
+        pass
 
     def stop(self):
         """Stop job for game"""
-        self.job.remove()
-
-    def stop_market(self):
-        """Stop job for game"""
-        self.market_job.remove()
+        if self.job is not None:
+            self.job.remove()
+            self.job = None
 
     def check(self):
         """Check for existing job"""
@@ -59,44 +35,69 @@ class Job():
             self.start()
 
 
-    def check(self):
-        """Check for existing job"""
-        if self.game.end_of_game and self.job is not None:
-            self.stop()
-        elif not self.game.end_of_game and self.job is None:
-            self.start()
+class Job(BaseJob):
+    """Class for handling scheduler"""
+
+    def __init__(self, game):
+        """Initialize Job"""
+        BaseJob.__init__(self, game)
+        self.game_id = str(game.game_id)
+        self.job = scheduler.get_job(self.game_id)
+
+    def start(self):
+        """Start job for game"""
+        if self.job is None:
+            self.job = scheduler.add_job(
+                id=self.game_id,
+                func=run,
+                args=[self.game.game_id],
+                trigger="interval",
+                days=1,
+                start_date=self.game.next_day_time + timedelta(minutes=5)
+            )
 
 
+class MarketJob(Job):
+    """Class for market job"""
 
-def run(game_id):
+    def __init__(self, game):
+        """Initialize MarketJob"""
+        Job.__init__(self, game)
+        self.job = scheduler.get_job("market_%s" % self.game_id)
+
+    def start(self):
+        """Start job"""
+        if self.job is None:
+            self.job = scheduler.add_job(
+                id="market_%s" % self.game_id,
+                func=run,
+                args=[self.game.game_id, "market"],
+                trigger="interval",
+                hours=1,
+            )
+
+
+def run(game_id, job_type=None):
     """Run the job"""
 
     game = Game.query.filter(Game.game_id == game_id).first()
 
     import sync
 
-    if game.track_game:
-        sync.update_game(game)
-    if game.track_players:
-        sync.update_players(game)
-    if game.track_score:
-        sync.update_score(game)
-    if game.track_relations:
-        sync.update_relations(game)
-    if game.track_coalitions:
-        sync.update_coalitions(game)
-
-
-def run_market(game_id):
-    """Run market job"""
-
-    game = Game.query.filter(Game.game_id == game_id).first()
-
-    import sync
-
-    sync.update_market(game)
+    if job_type == "market":
+        sync.update_market(game)
+    else:
+        if game.track_game:
+            sync.update_game(game)
+        if game.track_players:
+            sync.update_players(game)
+        if game.track_score:
+            sync.update_score(game)
+        if game.track_relations:
+            sync.update_relations(game)
+        if game.track_coalitions:
+            sync.update_coalitions(game)
 
 
 if __name__ == "__main__":
     run.__module__ = "scheduler"
-    run_market.__module__ = "scheduler"
