1fe16bc42d1c8768f71d980f0f8078a2c031b54c
JoostSijm <joostsijm@gmail.com>
2018-12-07 16:34:10 +0100

    Working on market job

--- app/util/job.py
+++ app/util/job.py
@@ -10,6 +10,7 @@ class Job():
 
     game = None
     job = None
+    market_job = None
     game_id = None
 
     def __init__(self, game):
@@ -17,6 +18,7 @@ class Job():
         self.game = game
         self.game_id = str(game.game_id)
         self.job = scheduler.get_job(self.game_id)
+        self.market_job = scheduler.get_job(self.game_id)
 
     def start(self):
         """Start job for game"""
@@ -29,9 +31,25 @@ class Job():
             start_date=self.game.next_day_time + timedelta(minutes=5)
         )
 
+
+    def start_market(self):
+        """Start job for market"""
+        scheduler.add_job(
+            id="market_%s" % self.game_id,
+            func=run_market,
+            args=[self.game.game_id],
+            trigger="interval",
+            hours=1,
+        )
+
+
     def stop(self):
         """Stop job for game"""
         self.job.remove()
+
+    def stop_market(self):
+        """Stop job for game"""
+        self.market_job.remove()
 
     def check(self):
         """Check for existing job"""
@@ -39,6 +57,15 @@ class Job():
             self.stop()
         elif not self.game.end_of_game and self.job is None:
             self.start()
+
+
+    def check(self):
+        """Check for existing job"""
+        if self.game.end_of_game and self.job is not None:
+            self.stop()
+        elif not self.game.end_of_game and self.job is None:
+            self.start()
+
 
 
 def run(game_id):
@@ -58,9 +85,18 @@ def run(game_id):
         sync.update_relations(game)
     if game.track_coalitions:
         sync.update_coalitions(game)
-    if game.track_market:
-        sync.update_market(game)
+
+
+def run_market(game_id):
+    """Run market job"""
+
+    game = Game.query.filter(Game.game_id == game_id).first()
+
+    import sync
+
+    sync.update_market(game)
 
 
 if __name__ == "__main__":
     run.__module__ = "scheduler"
+    run_market.__module__ = "scheduler"
