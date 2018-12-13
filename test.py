9955d9c52efb89707c513af54caea97779390d7a
JoostSijm <joostsijm@gmail.com>
2018-12-13 15:37:58 +0100

    Improve test function

--- test.py
+++ test.py
@@ -4,18 +4,34 @@ Test script
 """
 
 import sys
+import random
 from sqlalchemy.sql.expression import false
 from app.util import sync
 from app.models import Game
 
-# random game
-GAME_ID = 2527307
-if len(sys.argv) > 1:
-    GAME = Game.query.filter(Game.game_id == sys.argv[1]).first()
+if len(sys.argv) > 2:
+    # Search game by id
+    GAME = Game.query.filter(Game.game_id == sys.argv[2]).first()
 else:
+    # Get random game
     GAMES = Game.query.filter(Game.end_of_game == false()).all()
-    GAME = GAMES[0]
+    GAME = random.choice(GAMES)
 
-sync.update_market(GAME)
+SYNC_FUNCTIONS = {
+    "game": sync.update_game,
+    "players": sync.update_players,
+    "score": sync.update_score,
+    "relations": sync.update_relations,
+    "market": sync.update_market,
+    "coalitions": sync.update_coalitions,
+}
 
-print("\ndone!")
+if len(sys.argv) > 1:
+    if sys.argv[1] in SYNC_FUNCTIONS:
+        # Run function from argument
+        SYNC_FUNCTIONS[sys.argv[1]](GAME)
+    else:
+        print("%s is not a function" % sys.argv[1])
+else:
+    # Run random function
+    SYNC_FUNCTIONS[random.choice(list(SYNC_FUNCTIONS.keys()))](GAME)
