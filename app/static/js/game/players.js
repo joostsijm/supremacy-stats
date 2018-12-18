efbdcaf096eaf58acdaaf5cbf398ba7b311461b1
JoostSijm <joostsijm@gmail.com>
2018-12-18 14:45:49 +0100

    Fix sorting issue on players game

--- app/static/js/game/players.js
+++ app/static/js/game/players.js
@@ -21,22 +21,23 @@ $(document).ready(function() {
 			},
 		],
 	});
-});
 
-jQuery.extend( jQuery.fn.dataTableExt.oSort, {
-    'non-empty-string-asc': function (str1, str2) {
-        if(str1 == '')
-            return 1;
-        if(str2 == '')
-            return -1;
-        return ((str1 < str2) ? -1 : ((str1 > str2) ? 1 : 0));
-    },
- 
-    'non-empty-string-desc': function (str1, str2) {
-        if(str1 == '')
-            return 1;
-        if(str2 == '')
-            return -1;
-        return ((str1 < str2) ? 1 : ((str1 > str2) ? -1 : 0));
-    }
+	jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+	    'non-empty-string-asc': function (str1, str2) {
+	        if(str1 == '')
+	            return 1;
+	        if(str2 == '')
+	            return -1;
+	        return ((str1 < str2) ? -1 : ((str1 > str2) ? 1 : 0));
+	    },
+	 
+	    'non-empty-string-desc': function (str1, str2) {
+	        if(str1 == '')
+	            return 1;
+	        if(str2 == '')
+	            return -1;
+	        return ((str1 < str2) ? 1 : ((str1 > str2) ? -1 : 0));
+	    }
+	});
+
 });
